6df4322270ca5a5281a7dade94338e943e0820d2
Alexxx0-482 <70021451+Alexxx0-482@users.noreply.github.com>
2022-05-14 11:27:24 +1200

    Add files via upload

---
+++ prog1 A2/src/a2/GameState.java
@@ -0,0 +1,5 @@
+package a2;
+
+public enum GameState {
+	Playing, Draw, Cross_won, Nought_won
+}
---
+++ prog1 A2/src/a2/Player.java
@@ -0,0 +1,9 @@
+package a2;
+
+/**
+ * Enumeration for the players move
+ */
+
+public enum Player {
+	Empty, Cross, Nought
+}
